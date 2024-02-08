cefbea21ee0a740469dba8294db8f0958764e6b4
ArtemHvozdov <artemhvozdov@gmail.com>
2024-02-08 20:32:39 +0200

    create header

---
+++ src/layouts/main-screen/main-screen.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/header/header';
+import './main-screen.css'
+
+function MainScreen() {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default MainScreen;
