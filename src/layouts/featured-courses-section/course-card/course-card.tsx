b0bc7f761983dc99c8dc565e31f4139ba023e63c
Artem Hvozdov <113240736+ArtemHvozdov@users.noreply.github.com>
2024-02-17 23:50:17 +0200

    create course card component (#8)
    Co-authored-by: ArtemHvozdov <artemhvozdov@gmail.com>

---
+++ src/layouts/featured-courses-section/course-card/course-card.tsx
@@ -0,0 +1,11 @@
+import './course-card.css';
+
+function CourseCard() {
+    return (
+        <div className="courses__catalog-item">
+            <img src="" alt="" className="catalog__item-img" />
+        </div>
+    )
+}
+
+export default CourseCard;
